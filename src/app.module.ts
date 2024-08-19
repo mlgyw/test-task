ee398254aee8135f487611795a2f3a358b3333b5
mlgyw <kirya.gritsyk@mail.ru>
2024-08-19 11:02:22 +0300

    feat(): use prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
-
 @Module({
-  imports: [ConfigModule.forRoot(),
-    DatabaseModule,],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [],
   providers: [],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,20 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-
 @Module({
-	imports: [
-		ConfigModule.forRoot(),
-		MongooseModule.forRootAsync({
-			imports:[ConfigModule],
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => ({
-				uri: `${configService.get('MONGO_PREFIX')}://${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}/${configService.get('MONGO_DB_NAME')}`
-			})
-		})
-	],
-
-	
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: `${configService.get('MONGO_PREFIX')}://${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}/${configService.get('MONGO_DB_NAME')}`,
+      }),
+    }),
+  ],
 })
-export class DatabaseModule {
-}
+export class DatabaseModule {}
