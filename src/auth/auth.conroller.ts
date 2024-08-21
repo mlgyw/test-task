da195fec4bcbc41483ce8f298d6d91bcec1a1205
mlgyw <kirya.gritsyk@mail.ru>
2024-08-21 12:03:33 +0300

    feat(): init swagger, make swagger config, add swagger description , rebuild constants

--- src/auth/auth.conroller.ts
+++ src/auth/auth.conroller.ts
@@ -6,14 +6,29 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { AuthService } from '@/auth/auth.service';
-import { AuthSignInDto, AuthSignUpDto } from '@/auth/dto/auth.dto';
+import {
+  AuthSignInDto,
+  AuthSignUpDto,
+  AuthTokensDto,
+} from '@/auth/dto/auth.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Register' })
+  @ApiBody({
+    description: 'User data to create user',
+    type: AuthSignUpDto,
+  })
+  @ApiResponse({
+    status: 201,
+    type: AuthTokensDto,
+  })
   @Post('sign-up')
-  async signUp(@Body() authSignUpDto: AuthSignUpDto) {
+  async signUp(@Body() authSignUpDto: AuthSignUpDto): Promise<AuthTokensDto> {
     try {
       return await this.authService.register(authSignUpDto);
     } catch (err) {
@@ -21,8 +36,17 @@ export class AuthController {
     }
   }
 
+  @ApiOperation({ summary: 'Log-in' })
+  @ApiBody({
+    description: 'User data to get access',
+    type: AuthSignInDto,
+  })
+  @ApiResponse({
+    status: 201,
+    type: AuthTokensDto,
+  })
   @Post('sign-in')
-  async signIn(@Body() authSignInDto: AuthSignInDto) {
+  async signIn(@Body() authSignInDto: AuthSignInDto): Promise<AuthTokensDto> {
     try {
       return await this.authService.login(authSignInDto);
     } catch (err) {
--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -17,13 +17,41 @@ import { NoteDto, UpdateNoteDto, СreateNoteDto } from '@/note/dto/note.dto';
 import { JwtAuthGuard } from '@/auth/guards/jwt.guard';
 import { RequestWithUser } from '@/common/interfaces/request.interface';
 import { PaginationQueryDto } from '@/common/dto/pagination.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiExtraModels,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { ListResponse } from '@/common/types/list.type';
+import { NOTE_DOESNT_DELETE, NOTE_DOESNT_EXIST } from '@/note/note.constants';
 
+@ApiTags('notes')
 @Controller('notes')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('')
+  @ApiOperation({ summary: 'Create a new note' })
+  @ApiBody({
+    description: 'Note data to create a new note',
+    type: СreateNoteDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Note successfully created',
+    type: NoteDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request, invalid data',
+  })
+  @ApiBearerAuth()
   async create(
     @Body() createNoteDto: СreateNoteDto,
     @Req() req: RequestWithUser,
@@ -37,6 +65,22 @@ export class NoteController {
 
   @UseGuards(JwtAuthGuard)
   @Get('')
+  @ApiExtraModels(ListResponse, NoteDto)
+  @ApiOperation({ summary: 'Get all notes' })
+  @ApiBearerAuth()
+  @ApiOkResponse({
+    status: 200,
+    description: 'get all notes',
+    schema: {
+      allOf: [{ $ref: getSchemaPath(ListResponse) }],
+      properties: {
+        items: {
+          type: 'array',
+          items: { $ref: getSchemaPath(NoteDto) },
+        },
+      },
+    },
+  })
   async getList(
     @Req() req: RequestWithUser,
     @Query() paginationQuery: PaginationQueryDto,
@@ -50,6 +94,17 @@ export class NoteController {
 
   @UseGuards(JwtAuthGuard)
   @Get(':id')
+  @ApiOperation({ summary: 'Get note by ID' })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Note successfully retrieved',
+    type: NoteDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: NOTE_DOESNT_EXIST,
+  })
+  @ApiBearerAuth()
   async getOneByID(
     @Param('id') noteID: string,
     @Req() req: RequestWithUser,
@@ -63,6 +118,15 @@ export class NoteController {
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete note by ID' })
+  @ApiOkResponse({
+    status: 200,
+  })
+  @ApiResponse({
+    status: 400,
+    description: NOTE_DOESNT_DELETE,
+  })
+  @ApiBearerAuth()
   async deleteOneByID(
     @Param('id') noteID: string,
     @Req() req: RequestWithUser,
@@ -76,6 +140,21 @@ export class NoteController {
 
   @UseGuards(JwtAuthGuard)
   @Put(':id')
+  @ApiOperation({ summary: 'Create a new note' })
+  @ApiBody({
+    description: 'Note data to create a new note',
+    type: UpdateNoteDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Note successfully created',
+    type: NoteDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request, invalid data',
+  })
+  @ApiBearerAuth()
   async updateOneByID(
     @Param('id') noteID: string,
     @Req() req: RequestWithUser,
